45217efc7143c1f9f09f587494242955b6d53051
arceн <119044107+alyxmp4@users.noreply.github.com>
2022-11-25 17:49:08 +0500

    Add files via upload

---
+++ data/err/403.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ALYX WEBSITE | FORBIDDEN</title>
+  </head>
+  <body class="justify-content-center">
+    
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+        
+      body {
+        font-family: 'JetBrains Mono', monospace;
+      }
+    </style>
+    
+    <div class="card justify-content-center align-content-center text-bg-dark border text-center" style="width: 18rem; text-align: center; margin: auto; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); box-sizing: content-box;">
+      <div class="card-body">
+        <h5 class="card-title">403 Доступ запрещён</h5>
+        <p class="card-text">Доступ к данному документу запрещена, получить напрямую данные с этого файла невозможно.</p>
+        <div class="alert alert-danger" role="alert">
+          Вы будете перенаправлены на главную страницу через 10 секунд...
+        </div>
+    </div>
+    </div>
+    
+    <script>
+    
+    window.setTimeout(function() {
+      window.location.href = "https://postcards.alyxmp4.ml";
+    }, 10000);
+    
+    </script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
